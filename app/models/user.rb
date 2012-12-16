8ae7f089980f87dcb2e7b0b1d9e0d43dddcac53e
lgorse <lgorse@stanord.edu>
2012-12-16 02:55:32 -0800

    Password validation and encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+	attr_accessor :password
+  attr_accessible :email, :name, :password, :password_confirmation
 
 email_format = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, :presence=> true, :length => {:maximum => 45}
   validates :email, :presence=>true, 
 					:format => {:with=> email_format},
 					:uniqueness => {:case_sensitive => false}
+	validates :password, :presence => true,
+						 :confirmation => true,
+						 :length => {:within =>6..40}
 
 end
