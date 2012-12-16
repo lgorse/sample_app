ec84647d5452c02bef50948c70c85f4f144294bb
lgorse <lgorse@stanord.edu>
2012-12-16 12:32:05 -0800

    Password authentification model done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	attr_accessor :password
-  attr_accessible :email, :name, :password, :password_confirmation
+ 	attr_accessible :email, :name, :password, :password_confirmation
 
 email_format = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, :presence=> true, :length => {:maximum => 45}
@@ -11,4 +11,40 @@ email_format = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 						 :confirmation => true,
 						 :length => {:within =>6..40}
 
+ before_save :encrypt_password
+
+ def has_password?(submitted_password)
+ 	self.encrypted_password == encrypt(submitted_password)
+ end
+
+ class << self
+ 	def authenticate(email, submitted_password)
+ 		user = find_by_email(email)
+ 		return nil if user.nil?
+ 		return user if user.has_password?(submitted_password)
+ 	end
+
+ end
+
+ private 
+
+ def encrypt_password
+ 	self.salt = make_salt if new_record?
+ 	self.encrypted_password = encrypt(self.password)
+ end
+
+ def encrypt(string)
+ 	secure_hash("#{self.salt}--#{string}")
+ end
+
+ def secure_hash(string)
+ 	Digest::SHA2.hexdigest(string)
+ end
+
+ 	def make_salt
+ 		secure_hash("#{Time.now.utc}--#{self.password}")
+ 	end
+
 end
+
+#larry = User.create!(:name=>"larry", :email => "donald@duck.disney", :password=>"mickey", :password_confirmation=>"mickey")
